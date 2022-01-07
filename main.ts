db6eb948b58f2bf16ec714dbf2c6f62a2c3162cf
LiamMcCrory <93145772+LiamMcCrory@users.noreply.github.com>
2022-01-06 19:50:28 -0500

    Fichiers initiaux du projet MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 1e+67; index++) {
+    	
+    }
+})
+pins.digitalWritePin(DigitalPin.P0, 1)
+pins.digitalWritePin(DigitalPin.P1, 0)
+pins.digitalWritePin(DigitalPin.P2, 0)
+pins.digitalWritePin(DigitalPin.P8, 1)
+pins.digitalWritePin(DigitalPin.P16, 0)
